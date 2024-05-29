a173f12c2c8e982fc29141350810474e05d4e222
VictorGurgell <167359061+VictorGurgell@users.noreply.github.com>
2024-05-29 15:03:58 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+    $('.team-cards').slick({
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 2000,
+        dots: true,
+        arrows: true,
+    });
+});
